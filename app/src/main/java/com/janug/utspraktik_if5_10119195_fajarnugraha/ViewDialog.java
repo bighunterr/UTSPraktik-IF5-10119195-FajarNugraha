041ead14caed8e51bc74a28299643563a6fda93e
Fajar Nugraha <fajar.janug@gmail.com>
2022-06-02 22:48:35 +0700

    update

---
+++ app/src/main/java/com/janug/utspraktik_if5_10119195_fajarnugraha/ViewDialog.java
@@ -0,0 +1,30 @@
+package com.janug.utspraktik_if5_10119195_fajarnugraha;
+
+// Fajar Nugraha , 10119195, 2 JUni 2022
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ViewDialog {
+    public void showDialog(Activity activity, String msg){
+        final Dialog dialog = new Dialog(activity);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(false);
+        dialog.setContentView(R.layout.activity_view_dialog);
+
+        Button dialogButton = (Button) dialog.findViewById(R.id.ok);
+        dialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+
+    }
+}
